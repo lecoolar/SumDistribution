0ffdda18122115910961af33b24a8ba66f1f108b
lecoo <lecoo@DESKTOP-JB7N92L>
2022-11-18 18:29:00 +0300

    add reader

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using SumDistribution;
+﻿using SumDistribution;
 
-string type1 = "ПРОП";
-string type2 = "ПЕРВ";
-string type3 = "ПОСЛ";
-string sum = "10000";
-string sums = "1000;2000;3000;5000;8000;5000";
 
-Distribution distribution1 = new Distribution(type1, sum, sums);
-Distribution distribution2 = new Distribution(type2, sum, sums);
-Distribution distribution3 = new Distribution(type3, sum, sums);
+Console.Write("Введите тип распределения: ");
+string type = Console.ReadLine();
+Console.Write("Введите сумму для распределения: ");
+string sum = Console.ReadLine();
+Console.Write("Введите суммы для распределения(через запятую): ");
+string sums = Console.ReadLine();
+Distribution distribution = new Distribution(type, sum, sums);
+Console.WriteLine("{0} {1}", type, String.Join(";", distribution.GetDistribution()));
 
-Console.WriteLine("{0} {1}", type1, String.Join(";", distribution1.GetDistribution()));
-Console.WriteLine("{0} {1}", type2, String.Join(";", distribution2.GetDistribution()));
-Console.WriteLine("{0} {1}", type3, String.Join(";", distribution3.GetDistribution()));
-Console.WriteLine("Hello, World!");
